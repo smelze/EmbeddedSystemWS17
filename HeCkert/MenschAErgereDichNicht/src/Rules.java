54c7fad45d7ab7e912f71b2c98e758bd0d4d2829
mail <mail@10.170.24.104>
2017-11-21 13:06:49 +0100

    Erste Änderungen an Rules und main

--- HeCkert/MenschAErgereDichNicht/src/Rules.java
+++ HeCkert/MenschAErgereDichNicht/src/Rules.java
@@ -1,7 +1,16 @@
+//--- imports ----------------------------------------------------------------
+import java.util.Scanner;
+import java.util.ArrayList;
+
+  
 public class Rules {
-
+  
+//--- variables --------------------------------------------------------------
     private int diceResult;
+    private ArrayList<Player> PlayerList;
+    private GameBoard gameBoard;
 
+//--- functions -------------------------------------------------------------
     private boolean checkFieldStart() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
@@ -30,9 +39,41 @@ public class Rules {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    public void Rules() {
+    Rules() {
     }
 
     public void gameFlow() {
+        //--- local variables ----------------------------------------
+        Scanner scanner = new Scanner(System.in);
+        int playerCount,counter;
+        PlayerList = new ArrayList<>();
+        //------------------------------------------------------------
+        
+        //Spieler Anzahl bestimmen ... Spiel starten
+        System.out.println("Wie viele Spieler wollen Teilnehmen?");
+        playerCount = scanner.nextInt();//Eingabe von Buchstaben füht zu Absturz --- Besser allg. lesen und check Data --- auch noch abfangen max 4
+        System.out.println("Eingabe:" + String.valueOf(playerCount)); // Check Ausgabe kann später gelöscht werden
+       /*for(counter=0;counter<=playerCount;counter++){
+            switch(counter){
+                case 1:
+                    PlayerList.add(new Player("r"));
+                    break;
+                case 2:
+                    PlayerList.add(new Player("g"));
+                    break;
+                case 3:
+                    PlayerList.add(new Player("b"));
+                    break;
+                case 4:
+                    PlayerList.add(new Player("y"));
+                    break;
+                default:
+                    System.out.println("1 bis 4 Spieler möglich");
+                    break;
+            }    
+        }
+        gameBoard = new GameBoard(playerCount);
+        */
     }
 }
+
