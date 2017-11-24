a31b39b09ca1db99c91c3d1f0343802f2125d344
SvenjaLaptop <SvenjaLaptop@10.170.25.220>
2017-11-24 11:41:32 +0100

    Mensch geändert

--- HaLiHaHe/MenschAergereDichNicht/src/Mensch.java
+++ HaLiHaHe/MenschAergereDichNicht/src/Mensch.java
@@ -1,7 +1,14 @@
+import java.io.Console;
+
 public class Mensch extends Spieler {
 
-    private int FigurAuswaehlen() {
+    public Mensch(String name, int spielernummer) {
+        super(name, spielernummer);
+    }
+
+    @Override  int FigurAuswaehlen() {
+        System.out.print("Bitte waehlen Sie die Nummer der Figur aus, die Sie bewegen möchten:");
         
-        
+        Console.ReadLine()
     }
 }
