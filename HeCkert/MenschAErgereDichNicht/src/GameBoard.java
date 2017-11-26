f30074de38f87abe2d7d9e07b725dfcf3bf1cdef
Sebastian <Sebastian@10.170.21.227>
2017-11-26 23:22:37 +0100

    added UI functionality

--- HeCkert/MenschAErgereDichNicht/src/GameBoard.java
+++ HeCkert/MenschAErgereDichNicht/src/GameBoard.java
@@ -10,8 +10,8 @@ Positions 16 - 55 represent the circle track.
 Positions 16(r), 26(b), 36(g) and 46(y) represent the start points of 
 red, blue, green and yellow respectively.
 
-Positions 56 - 72 represent the "home" areas of the board.
-Positions 56-59(r), 60-63(g), 64-57(b) and 58-61(y) represent the home areas of 
+Positions 56 - 71 represent the "home" areas of the board.
+Positions 56-59(r), 60-63(g), 64-67(b) and 68-71(y) represent the home areas of 
 red, green, blue and yellow respectively
 */
 
@@ -22,6 +22,7 @@ public class GameBoard extends JFrame{
 
     private ArrayList<Meeple> MeepleList;
     private ListIterator<Meeple> MeepleIterator;
+    private MenschAErgereDichNichtPanel Panel;
     
     /**
      * <code>GameBoard</code> has to be initialized with 2 - 4 Players.
@@ -38,9 +39,9 @@ public class GameBoard extends JFrame{
             char color = 'y';
             
             for(int i=0; i<(4*playerCount); i++){
-                if(i<=4) color = 'r';
-                else if(i<=8) color = 'g';
-                else if (i<=12) color = 'b';
+                if(i<=3) color = 'r';
+                else if(i<=7) color = 'g';
+                else if (i<=11) color = 'b';
                 MeepleList.add(new Meeple(color,i));
             }
         }
@@ -48,8 +49,8 @@ public class GameBoard extends JFrame{
     }
     
         private void initUI() {
-
-        add(new MaenschAErgereDichNichtPanel());
+            Panel = new MenschAErgereDichNichtPanel();
+        add(Panel);
 
         pack();
 
@@ -97,16 +98,16 @@ public class GameBoard extends JFrame{
         int position;
         switch(color){
             case 'r':
-                position = 47;
+                position = 56;
                 break;
             case 'g':
-                position = 51;
+                position = 60;
                 break;
             case 'b':
-                position = 55;
+                position = 64;
                 break;
             case 'y':
-                position = 59;
+                position = 68;
                 break;
             default:
                 position = -1;
@@ -148,10 +149,12 @@ public class GameBoard extends JFrame{
      * @param newPosition 
      */
     public void moveMeeple(int startposition, int newPosition) {
+        MeepleIterator = MeepleList.listIterator(0);
         while(MeepleIterator.hasNext()){
             Meeple m = MeepleIterator.next();
             if(m.getPosition() == startposition ){
-                m.setPosition(newPosition) ;
+                m.setPosition(newPosition);
+                Panel.drawMeepleByColor(m.getColor(),newPosition);
             }            
         }
     }
@@ -164,6 +167,7 @@ public class GameBoard extends JFrame{
      * Returns an int array with four entrys.
      */
     public int[] checkMeeple(char color) {
+        MeepleIterator = MeepleList.listIterator(0);
         int[] positions = new int[4];
         int i = 0;
         while(MeepleIterator.hasNext()){
---
+++ HeCkert/MenschAErgereDichNicht/src/MenschAErgereDichNichtPanel.java
@@ -0,0 +1,124 @@
+
+import java.awt.AlphaComposite;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class MenschAErgereDichNichtPanel extends JPanel {
+
+        private BufferedImage background;
+        private final int[][] layout = 
+           {{0,1,-1,-1,24,25,26,-1,-1,8,9},
+            {2,3,-1,-1,23,55,27,-1,-1,10,11},
+            {-1,-1,-1,-1,22,56,28,-1,-1,-1,-1},
+            {-1,-1,-1,-1,21,57,29,-1,-1,-1,-1},
+            {16,17,18,19,20,58,30,31,32,33,34},
+            {55,56,57,58,59,-1,63,62,61,60,35},
+            {54,53,52,51,50,71,40,39,38,37,36},
+            {-1,-1,-1,-1,49,70,41,-1,-1,-1,-1},
+            {-1,-1,-1,-1,48,69,42,-1,-1,-1,-1},
+            {12,13,-1,-1,47,68,43,-1,-1,4,5},
+            {14,15,-1,-1,46,45,44,-1,-1,6,7}};
+        Graphics g;
+
+    public MenschAErgereDichNichtPanel() {
+        //TODO: Change path to resources
+        URL resource = getClass().getResource("386px-Menschenaergern.jpg");
+        try {
+            background = ImageIO.read(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void drawMeepleByColor(char color, int position){
+        Graphics2D Circle = background.createGraphics();
+        int xpos=0,ypos=0;
+        
+        for(int y=0; y<11; y++){
+            for(int x=0; x<11; x++){
+                if(layout[y][x]==position){
+                    xpos = 15+(x*34);
+                    ypos = 16+(y*34);
+                }
+            }
+        }
+        Circle.setStroke(new BasicStroke(5));
+        Circle.setColor(Color.BLACK);
+        Circle.drawArc(xpos, ypos, 15, 15, 0, 360);
+        switch(color){
+            case 'r':
+                Circle.setColor(Color.RED);
+                break;
+            case 'g':
+                Circle.setColor(Color.GREEN);
+                break;
+            case 'b':
+                Circle.setColor(Color.BLUE);
+                break;
+            case 'y':
+                Circle.setColor(Color.YELLOW);
+                break;
+            default:
+                Circle.setColor(Color.BLACK);
+                break;                
+        }
+        Circle.fillArc(xpos, ypos, 15, 15, 0, 360);
+        clear(xpos, ypos);
+    }
+    
+    private void clear(int xpos, int ypos){
+        final Graphics2D a = background.createGraphics();
+        a.setColor(new Color(0, 0, 0, 0));
+        a.setComposite(AlphaComposite.Clear); // overpaint
+        a.setComposite(AlphaComposite.SrcOver);
+        a.clearRect(xpos, ypos, 16, 16);
+        a.dispose();
+        repaint();        
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int w = background.getWidth(this);
+        int h =  background.getHeight(this);
+        this.setSize(new Dimension(w, h)); 
+        g.drawImage(background, 0, 0, this);
+    }
+    /*private Image background;
+
+    public MenschAErgereDichNichtPanel() {
+
+        initBoard();
+    }
+    
+    private void initBoard() {
+        
+        loadImage();
+        
+        int w = background.getWidth(this);
+        int h =  background.getHeight(this);
+        setPreferredSize(new Dimension(w, h));        
+    }
+    
+    private void loadImage() {
+        
+        ImageIcon ii = new ImageIcon("386px-Menschenaergern.png");
+        background = ii.getImage();        
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        g.drawImage(background, 0, 0, null);
+    }*/
+}
