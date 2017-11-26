35f3ebbcfd3478645a55d4dbec84987a5e7e5a6c
mail <mail@10.170.24.104>
2017-11-26 13:36:29 +0100

    implements roll dice and some changes within rules

--- HeCkert/MenschAErgereDichNicht/src/Rules.java
+++ HeCkert/MenschAErgereDichNicht/src/Rules.java
@@ -27,30 +27,28 @@ public class Rules {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    public char rollDice(char color) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+    public int rollDice() {
+        return (int)(Math.random()*10);
+    }// End of rollDice()
 
     private char checkForcedActionSix(char color) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    public int[] checkPossibleMoves() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+    public int[] checkPossibleMoves(int diceCount,int playerCounter) {
+        checkForcedAction();
+    }//End of checkPossibleMoves()
 
     Rules() {
-    }
+    }//End of Rules() constructor
 
-    public void gameFlow() {
+    public void initGame() {
         //--- local variables ----------------------------------------
         Scanner scannerInt = new Scanner(System.in);
         Scanner scannerLine = new Scanner(System.in);
         int playerCount,counter;
         PlayerList = new ArrayList<>();
         //------------------------------------------------------------
-        
-        //Spieler Anzahl bestimmen ... Spiel starten
         System.out.println("Wie viele Spieler wollen Teilnehmen?");
         playerCount = scannerInt.nextInt();//Eingabe von Buchstaben füht zu Absturz --- Besser allg. lesen und check Data --- auch noch abfangen max 4
         System.out.println("Eingabe:" + String.valueOf(playerCount)); // Check Ausgabe kann später gelöscht werden
@@ -71,16 +69,31 @@ public class Rules {
                 case 4:
                     System.out.println("Bitte einen Namen für Spieler " + counter + " eingeben:");
                     PlayerList.add(new Player('y',scannerLine.nextLine()));
-                    System.out.println("Dies ist eine sinnlose Testausgabe");
                     break;
                 default:
                     System.out.println("1 bis 4 Spieler möglich...Hier lief etwas falsch...suche den Fehler ;-P");
                     break;
-                    //Sinnloser Testlkommentar zwecks übertragungstest
             }    
         }
         gameBoard = new GameBoard(playerCount);
-        
-    }
+        loopGame();
+    }//End of function initGame()
+    private void turn(){
+    
+    }//End of function turn()
+    
+    public void loopGame(){
+        //--- local variables -----------
+        int counter=0, diceCount, choosedPosition;
+        //-------------------------------
+        do{
+            //System.out.println("Player " + (counter+1) + " heißt " + PlayerList.get(counter).getName());
+            PlayerList.get(counter).startTurn();
+            diceCount=rollDice();
+            choosedPosition = PlayerList.get(counter).chooseField(checkPossibleMoves(diceCount,counter));
+            gameBoard.moveMeeple(choosedPosition, (choosedPosition + diceCount));
+            counter++;
+        }while(counter < PlayerList.size());
+    }// End of function loopGame()
 }
 
